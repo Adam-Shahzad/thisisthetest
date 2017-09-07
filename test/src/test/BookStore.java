3b1b44fc932ab0281172857a829ad4fe584b0212
Adam-Shahzad <adam.shahzad@outlook.com>
2017-09-07 13:45:04 +0000

    realised i was doing the wrong thing

--- test/src/test/BookStore.java
+++ test/src/test/BookStore.java
@@ -5,16 +5,31 @@ import java.util.ArrayList;
 public class BookStore {
 	
  
- public ArrayList<GenreResult> GenreCounter(){
+ public int GenreCounter(String genreSearch){
+	 int countGenre = 0;
+	 ArrayList<Book> books = new ArrayList<Book>();
+	 
 	 
 	 Book aBook = new Book("a","a",1,"a");
 	 Book bBook = new Book("b","b",2,"a");
-	 Book cBook = new Book("b","b",3,"c");
-	 Book dBook = new Book("b","b",4,"c");
-	 Book eBook = new Book("b","b",5,"c");
+	 Book cBook = new Book("c","c",3,"c");
+	 Book dBook = new Book("d","d",4,"c");
+	 Book eBook = new Book("e","e",5,"c");
 	 
 	 
-	 ArrayList<GenreResult> GenreCount = new ArrayList<GenreResult>();
-	 return GenreCount;
+	 books.add(aBook);
+	 books.add(bBook);
+	 books.add(cBook);
+	 books.add(dBook);
+	 books.add(eBook);
+	 
+	
+	 for(Book b : books){
+		 if (b.getBook_genre() == genreSearch){
+			 countGenre++;
+		 }
+	 }
+	 
+	 return countGenre;
  }
 }
--- test/src/test/Testy.java
+++ test/src/test/Testy.java
@@ -11,13 +11,7 @@ public class Testy {
 	@Test
 	public void test() {
 		BookStore b = new BookStore();
-		
-		ArrayList<GenreResult> expected = new ArrayList<GenreResult>();
-		
-		expected.add(new GenreResult("a",2));
-		expected.add(new GenreResult("c",3));
-		
-		assertEquals(b.GenreCounter(),expected);
+		assertEquals(b.GenreCounter("a"),2);
 	}
 
 }
